dd7c02fc6a41437396579a45e6ac5a641b80d181
KWONYUBIN <rnjsdbqls112@naver.com>
2024-08-14 16:56:14 +0900

    변수명 작성 관습

---
+++ src/ch02/lecture/p02type/C01Type.java
@@ -0,0 +1,41 @@
+package ch02.lecture.p02type;
+
+public class C01Type {
+        public static void main(String[] args) {
+
+// 1. primitive type 8가지
+            //  byte : 1바이트 정수
+            // short : 2바이트 정수
+            // int : 4바이트 정수*
+            // long : 8바이트 정수*
+            // float : 4바이트 실수*
+            // double : 8바이트 실수*
+            // boolean : 1바이트 논리*
+            // char : 1바이트 문자
+
+            // 2.
+            //
+
+            ///////////////////////////
+            // primitive type
+            // 정수형 (byte(1), short(2), int(4), long(8))
+            byte b1;
+            b1 = 127;
+            byte b2;
+            b2 = -128;
+
+      //    byte b3 = 128; //x
+      //
+
+        // long (8byte)
+        // 4byte 이상의 수 작성 시 끝에 L 붙이기
+            long var1 = 10;
+            long var2 = 20L;
+            // long var3 = 1000000000000;  long를 붙이고 싶으면 뒤에 L을 붙여야됨
+            long var4 = 1000000000000L;
+
+
+
+
+        }
+}
---
+++ src/ch02/lecture/p02type/C03Double.java
@@ -0,0 +1,15 @@
+package ch02.lecture.p02type;
+
+import java.math.BigDecimal;
+
+public class C03Double {
+    public static void main(String[] args){
+        double a = 0.1;
+        double b = 0.2;
+        double c = a + b;
+
+        System.out.println("c = " + c);
+
+
+    }
+}
