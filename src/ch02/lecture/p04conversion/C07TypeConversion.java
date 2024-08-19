4d345e2e2e106f8c6b735df2142d86cd51a7ae70
KWONYUBIN <rnjsdbqls112@naver.com>
2024-08-19 17:23:10 +0900

    0819

--- src/ch02/lecture/p04conversion/C07TypeConversion.java
+++ src/ch02/lecture/p04conversion/C07TypeConversion.java
@@ -7,10 +7,9 @@ public class C07TypeConversion {
         int b = (int) a; // 기본타입끼리의 캐스팅 가능
 
         //
-        //   String c = (String) a; //
+//        String c = (String) a; //
         String d = "3";
-        //   int e = (int) d; // 기본타입과 참조타입간의 형변환 x
-
+//        int e = (int) d; // 기본타입과 참조타입간의 형변환 x
 
     }
 }
--- src/ch02/lecture/p04conversion/C09Var.java
+++ src/ch02/lecture/p04conversion/C09Var.java
@@ -5,7 +5,9 @@ public class C09Var {
         // 키워드는 변수명(심블)으로 사용 불가
         int i = 3;
 
-        int var = 3;
+        int var = 3; // var는 변수명(심볼)로 사용 가능
+
+        var j = 5;
 
 
     }
