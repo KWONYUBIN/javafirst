dc31a3180211ef5ae6d26990c2755394cbee1ade
KWONYUBIN <rnjsdbqls112@naver.com>
2024-08-22 17:15:32 +0900

    0822

---
+++ src/ch5/lecture/p01array/C07Array.java
@@ -0,0 +1,24 @@
+package ch5.lecture.p01array;
+
+public class C07Array {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 99;
+        arr[1] = 88;
+        arr[2] = 77;
+
+        // 배열의 길이와 초기값을 한 번에
+        int[] brr;
+        brr = new int[]{55, 44, 33, 22};
+
+        for (int i = 0; i < brr.length; i++) {
+            System.out.println("brr[i] = " + brr[i]);
+
+//            for (int j : brr) {
+//                System.out.println("brr[i] = " +j);    --> 향샹된 for문
+        }
+
+    }
+
+}
+
