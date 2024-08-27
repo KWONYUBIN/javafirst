d3d98d34120b3fb7e92fb5e16fbfd0e7f2e31ec9
KWONYUBIN <rnjsdbqls112@naver.com>
2024-08-27 17:50:32 +0900

    yubin1

---
+++ yubinjava/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,11 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        // 변수 value 선언
+        int value = 10;
+        int result = value + 10;
+        System.out.println(result);
+
+    }
+}
