68944cc88c73de50026f055692d1cbedc55b58fc
KWONYUBIN <rnjsdbqls112@naver.com>
2024-09-20 17:49:39 +0900

    0920

--- src/ch18/sol10/CopyFileExample.java
+++ src/ch18/sol10/CopyFileExample.java
@@ -1,17 +1,42 @@
 package ch18.sol10;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 
 public class CopyFileExample {
     public static void main(String[] args) {
+        try {
+            Reader in = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(in);
 
-        File file1 = new File("C:/Temp/dir1/photo1.jpg");
-        File file2 = new File("C:/Temp/dir2/photo2.jpg");
+            try (br; in) {
+                System.out.println("원본 파일 경로");
+                String sourceFileName = br.readLine();
+                System.out.println("복사 파일 경로");
+                String copyFileName = br.readLine();
+                File sourceFile = new File(sourceFileName);
+                File copyFile = new File(copyFileName);
+                if (sourceFile.exists()) {
+                    File dir = copyFile.getParentFile();
+                    if (!dir.exists()) {
+                        dir.mkdir();
+                    }
+                    FileInputStream fis = new FileInputStream(sourceFile);
+                    FileOutputStream fos = new FileOutputStream(copyFile);
+                    BufferedInputStream is = new BufferedInputStream(fis);
+                    BufferedOutputStream os = new BufferedOutputStream(fos);
+                    try (os; is; fos; fis;) {
+                        is.transferTo(os);
+                        os.flush();
+                    }
+                    System.out.println("복사가 성공하였습니다.");
+                } else {
+                    System.out.println("원본 파일이 존재하지 않습니다.");
+                }
+            }
 
-        if (file1.exists() == false) {
-            System.out.println("원본 파일이 존재하지 않습니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
